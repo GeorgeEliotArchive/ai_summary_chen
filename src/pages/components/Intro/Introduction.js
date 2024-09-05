49424c17d6baaadfb5f01a404012d41046047907
yuecui <yzc0058@auburn.edu>
2024-09-05 08:25:53 -0500

    Update Introduction.js

--- src/pages/components/Intro/Introduction.js
+++ src/pages/components/Intro/Introduction.js
@@ -14,41 +14,7 @@ const Introduction = () => {
         AI Text Analysis is a compilation of AI-generated, chapter-by-chapter
         text analysis of George Eliot's major works of fiction.
       </p>
-      // <p>
-      //   We at the George Eliot Archive were excited by the potential of ChatGPT
-      //   to provide detailed summaries of Eliot's works. We began using the open
-      //   access program to extract characters, themes, significant quotations,
-      //   and generate summaries at the micro (by chapter) and macro level (by
-      //   book or complete text). On inspecting these results, we found them full
-      //   of errors. Even the chapter titles were often incorrect.
-      // </p>
-      // <p>
-      //   But then we learned we could access GPT-4 by subscription and generate
-      //   those same requests after feeding in nothing but George Eliot's texts.
-      //   Instead of the entire internet, the program would analyze only the
-      //   original document. It worked! It was able to generate reliable chapter
-      //   summaries and extract all the information we requested, if it was
-      //   included in the text.
-      // </p>
-      // <p>
-      //   We plan to use these results to train our Text Explorer tools but in the
-      //   meantime, we wanted to share all the data we have paid for and amassed
-      //   in hopes that it will be useful to others.
-      // </p>
-      // <p>
-      //   <b>To see the chapter-by-chapter AI-generated analytics,</b> first,
-      //   click on a novel in the top row, then click on any chapter in the left
-      //   column. The extracted data is at the top of the text bok, with the
-      //   original source text below. These texts are searchable and downloadable,
-      //   for your convenience.
-      // </p>
-      // <p>
-      //   Please let us know what you are doing with this data. It's an exciting
-      //   time to be playing in the field of digital humanities!
-      // </p>
-      // <p>
-      //   Please send your comments and suggestions to Bev: BRilett2@Auburn.edu
-      // </p>
+     
     </div>
   );
 }
